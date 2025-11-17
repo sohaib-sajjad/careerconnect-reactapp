6ab92564b4d17387c2c6395bf92dc4184ce13198
sohaib-sajjad <91666222+sohaib-sajjad@users.noreply.github.com>
2025-11-17 14:31:41 +0000

    defined common styling for div across the app

---
+++ src/styles.js
@@ -0,0 +1,11 @@
+export const styles = {
+app: {
+    fontFamily:
+      "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', sans-serif",
+    background: "#f4f5fb",
+    minHeight: "100vh",
+    padding: "20px",
+    boxSizing: "border-box",
+  }
+  
+}
